4086039f06b45b504a540f14283d3c6a4734a083
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2024-10-06 22:21:45 +0545

    some function

--- Pytorch/Classification/function.py
+++ Pytorch/Classification/function.py
@@ -1,8 +1,3 @@
-"""
-A series of helper functions used throughout the course.
-
-If a function gets defined once and could be used over and over, it'll go in here.
-"""
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,26 +16,10 @@ import requests
 import os
 
 def walk_through_dir(dir_path):
-    """
-    Walks through dir_path returning its contents.
-    Args:
-    dir_path (str): target directory
-
-    Returns:
-    A print out of:
-      number of subdiretories in dir_path
-      number of images (files) in each subdirectory
-      name of each subdirectory
-    """
     for dirpath, dirnames, filenames in os.walk(dir_path):
         print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
 
 def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
-    """Plots decision boundaries of model predicting on X in comparison to y.
-
-    Source - https://madewithml.com/courses/foundations/neural-networks/ (with modifications)
-    """
-    # Put everything to CPU (works better with NumPy + Matplotlib)
     model.to("cpu")
     X, y = X.to("cpu"), y.to("cpu")
 
@@ -75,9 +54,6 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
 def plot_predictions(
     train_data, train_labels, test_data, test_labels, predictions=None
 ):
-    """
-  Plots linear training data and test data and compares predictions.
-  """
     plt.figure(figsize=(10, 7))
 
     # Plot training data in blue
@@ -96,31 +72,12 @@ def plot_predictions(
 
 # Calculate accuracy (a classification metric)
 def accuracy_fn(y_true, y_pred):
-    """Calculates accuracy between truth labels and predictions.
-
-    Args:
-        y_true (torch.Tensor): Truth labels for predictions.
-        y_pred (torch.Tensor): Predictions to be compared to predictions.
-
-    Returns:
-        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
-    """
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
 
 
 def print_train_time(start, end, device=None):
-    """Prints difference between start and end time.
-
-    Args:
-        start (float): Start time of computation (preferred in timeit format). 
-        end (float): End time of computation.
-        device ([type], optional): Device that compute is running on. Defaults to None.
-
-    Returns:
-        float: time between start and end in seconds (higher is longer).
-    """
     total_time = end - start
     print(f"\nTrain time on {device}: {total_time:.3f} seconds")
     return total_time
@@ -128,15 +85,6 @@ def print_train_time(start, end, device=None):
 
 # Plot loss curves of a model
 def plot_loss_curves(results):
-    """Plots training curves of a results dictionary.
-
-    Args:
-        results (dict): dictionary containing list of values, e.g.
-            {"train_loss": [...],
-             "train_acc": [...],
-             "test_loss": [...],
-             "test_acc": [...]}
-    """
     loss = results["train_loss"]
     test_loss = results["test_loss"]
 
@@ -177,26 +125,6 @@ def pred_and_plot_image(
     transform=None,
     device: torch.device = "cuda" if torch.cuda.is_available() else "cpu",
 ):
-    """Makes a prediction on a target image with a trained model and plots the image.
-
-    Args:
-        model (torch.nn.Module): trained PyTorch image classification model.
-        image_path (str): filepath to target image.
-        class_names (List[str], optional): different class names for target image. Defaults to None.
-        transform (_type_, optional): transform of target image. Defaults to None.
-        device (torch.device, optional): target device to compute on. Defaults to "cuda" if torch.cuda.is_available() else "cpu".
-    
-    Returns:
-        Matplotlib plot of target image and model prediction as title.
-
-    Example usage:
-        pred_and_plot_image(model=model,
-                            image="some_image.jpeg",
-                            class_names=["class_1", "class_2", "class_3"],
-                            transform=torchvision.transforms.ToTensor(),
-                            device=device)
-    """
-
     # 1. Load in image and convert the tensor values to float32
     target_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
 
@@ -237,11 +165,6 @@ def pred_and_plot_image(
     plt.axis(False)
 
 def set_seeds(seed: int=42):
-    """Sets random sets for torch operations.
-
-    Args:
-        seed (int, optional): Random seed to set. Defaults to 42.
-    """
     # Set the seed for general torch operations
     torch.manual_seed(seed)
     # Set the seed for CUDA torch operations (ones that happen on the GPU)
@@ -250,20 +173,6 @@ def set_seeds(seed: int=42):
 def download_data(source: str, 
                   destination: str,
                   remove_source: bool = True) -> Path:
-    """Downloads a zipped dataset from source and unzips to destination.
-
-    Args:
-        source (str): A link to a zipped file containing data.
-        destination (str): A target directory to unzip data to.
-        remove_source (bool): Whether to remove the source after downloading and extracting.
-    
-    Returns:
-        pathlib.Path to downloaded data.
-    
-    Example usage:
-        download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
-                      destination="pizza_steak_sushi")
-    """
     # Setup path to data folder
     data_path = Path("data/")
     image_path = data_path / destination
